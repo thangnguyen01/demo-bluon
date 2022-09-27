a437655256a8b5e287e5b3dd82c5db8fc12aa6cd
Phong Huy <>
2022-09-28 01:44:00 +0700

    Refactor project endpoint

--- src/apis/user/user.module.ts
+++ src/apis/user/user.module.ts
@@ -1,5 +1,5 @@
-import { ApiController } from './api/api.controller';
-import { ApiService } from './api/api.service';
+import { AgoraController } from './agora/agora.controller';
+import { AgoraControllerService } from './agora/agora.service';
 import { FCMController } from './fcm/fcm.controller';
 import { FCMTokenModule } from 'src/core/infrastructure/fcm/fcm-token.module';
 import { FireBaseModule } from 'src/core/firebase/firebase.module';
@@ -13,11 +13,11 @@ import { Module } from '@nestjs/common';
     FCMTokenModule,
   ],
   providers: [
-    ApiService,
+    AgoraControllerService,
   ],
   controllers: [
-    ApiController,
+    AgoraController,
     FCMController,
   ]
 })
-export class UserAPIModule { }
+export class UserModule { }
---
+++ src/app-router.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import UserRouterMoudle from './apis/user/user-router.module';
+
+@Module({
+  imports: [
+    UserRouterMoudle
+  ]
+})
+export class AppRouterModule { }
