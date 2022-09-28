23440e398b6e12439274fd4b42b194b2a0a2802b
anthonyngg192 <thang.nguyen@nucleusstudio.io>
2022-09-28 09:55:06 +0700

    update dto for add token

--- src/apis/user-api/fcm/dto/add-token.dto.ts
+++ src/apis/user-api/fcm/dto/add-token.dto.ts
@@ -1,13 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { FcmType } from 'src/core/infrastructure/fcm/constants/fcm.type';
+import { IsNotEmpty } from 'class-validator';
 
 export class AddTokenDTO {
   @ApiProperty()
+  @IsNotEmpty()
   userId: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   token: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   type: FcmType
 }
